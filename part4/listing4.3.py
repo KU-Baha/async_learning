9fe6a12c8f2f26cdd35f63b99207b7067b7c08b4
ku-baha <kulbarakovbh@gmail.com>
2024-03-09 19:39:16 +0600

    Timeouts

---
+++ part4/listing4.3.py
@@ -0,0 +1,26 @@
+import asyncio
+import aiohttp
+
+from aiohttp import ClientSession
+from async_learn.utils import async_timed
+
+
+@async_timed()
+async def fetch_status(session: ClientSession, url: str) -> int:
+    ten_millis = aiohttp.ClientTimeout(total=.01)
+
+    async with session.get(url, timeout=ten_millis) as response:
+        return response.status
+
+
+@async_timed()
+async def main():
+    session_timeout = aiohttp.ClientTimeout(total=1, connect=.1)
+
+    async with aiohttp.ClientSession(timeout=session_timeout) as session:
+        url = 'https://www.google.com'
+        status = await fetch_status(session, url)
+        print(f'Статус {url}: {status}')
+
+
+asyncio.run(main())
